e6c72cf5807bacd01d4de6cabe919ef8269d4d9d
Kirana Cinta <kiranamentari0123@gmail.com>
2026-06-04 14:40:46 +0700

    login dan dashboard masih eror

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
+session_start();
+include "koneksi.php";
+
+// cek apakah user sudah login
+if (!isset($_SESSION["login"])) {
+    header("Location: login.php");
+    exit;
+}
+?>
+
+<?php
 include "koneksi.php";
 date_default_timezone_set('Asia/Jakarta');
 
